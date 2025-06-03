6e8aa824ef5174bb11b0179c9e398ad3c6af7045
pmgl <gilles@pommereuil.com>
2025-06-03 15:54:50 +0200

    ip blacklist

---
+++ server/banip.coffee
@@ -0,0 +1,51 @@
+fs = require('fs')
+path = require('path')
+
+class KnownIP
+  constructor:(@banip,@ip)->
+    @last_requests = [0,0,0,0]
+    @index = 0
+
+  request:()->
+    time = Date.now()
+    @last_requests[@index] = time
+    @index = (@index+1) % @last_requests.length
+    dt = time - @last_requests[@index]
+    if dt < 500
+      @ban()
+
+  ban:()->
+    @banip.banIP(@ip)
+    @banned = true
+
+BLOCKED_IPS_FILE = path.join(__dirname, 'blocked_ips.txt');
+
+class @BanIP
+  constructor:(@server)->
+    @known_ips = {}
+    @banned = {}
+
+    if fs.existsSync( BLOCKED_IPS_FILE )
+      lines = fs.readFileSync(BLOCKED_IPS_FILE, 'utf-8').split('\n')
+      for ip in lines
+        @banned[ip] = true
+
+    @write_stream = fs.createWriteStream(BLOCKED_IPS_FILE, { flags: 'a' })
+
+  banIP:(ip)->
+    @banned[ip] = true
+    try
+      @write_stream.write(ip + '\n');
+    catch err
+
+  isBanned:(ip)->
+    return @banned[ip] or false
+
+  request:(ip)->
+    knownip = @known_ips[ip]
+    if not knownip
+      knownip = @known_ips[ip] = new KnownIP @, ip
+
+    knownip.request()
+
+module.exports = @BanIP
---
+++ server/banip.js
@@ -0,0 +1,78 @@
+var BLOCKED_IPS_FILE, KnownIP, fs, path;
+
+fs = require('fs');
+
+path = require('path');
+
+KnownIP = class KnownIP {
+  constructor(banip, ip1) {
+    this.banip = banip;
+    this.ip = ip1;
+    this.last_requests = [0, 0, 0, 0];
+    this.index = 0;
+  }
+
+  request() {
+    var dt, time;
+    time = Date.now();
+    this.last_requests[this.index] = time;
+    this.index = (this.index + 1) % this.last_requests.length;
+    dt = time - this.last_requests[this.index];
+    if (dt < 500) {
+      return this.ban();
+    }
+  }
+
+  ban() {
+    this.banip.banIP(this.ip);
+    return this.banned = true;
+  }
+
+};
+
+BLOCKED_IPS_FILE = path.join(__dirname, 'blocked_ips.txt');
+
+this.BanIP = class BanIP {
+  constructor(server) {
+    var i, ip, len, lines;
+    this.server = server;
+    this.known_ips = {};
+    this.banned = {};
+    if (fs.existsSync(BLOCKED_IPS_FILE)) {
+      lines = fs.readFileSync(BLOCKED_IPS_FILE, 'utf-8').split('\n');
+      for (i = 0, len = lines.length; i < len; i++) {
+        ip = lines[i];
+        this.banned[ip] = true;
+      }
+    }
+    this.write_stream = fs.createWriteStream(BLOCKED_IPS_FILE, {
+      flags: 'a'
+    });
+  }
+
+  banIP(ip) {
+    var err;
+    this.banned[ip] = true;
+    try {
+      return this.write_stream.write(ip + '\n');
+    } catch (error) {
+      err = error;
+    }
+  }
+
+  isBanned(ip) {
+    return this.banned[ip] || false;
+  }
+
+  request(ip) {
+    var knownip;
+    knownip = this.known_ips[ip];
+    if (!knownip) {
+      knownip = this.known_ips[ip] = new KnownIP(this, ip);
+    }
+    return knownip.request();
+  }
+
+};
+
+module.exports = this.BanIP;
